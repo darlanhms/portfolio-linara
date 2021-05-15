3b33c203f16602a91ca90e7612a264f5490e21a4
Darlan H <darlanhermessjb@hotmail.com>
2021-05-14 21:59:46 -0300

    troca de imagem de em breve

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,8 +5,7 @@ import styles from '../styles/Home.module.css';
 
 const Home = (): React.ReactElement => (
   <div className={styles.container}>
-    <Image width={500} height={500} src="/images/building.jpg" />
-    <h1 className={styles.text}>Em breve...</h1>
+    <Image width={600} height={600} src="/images/em_breve.png" />
   </div>
 );
 
