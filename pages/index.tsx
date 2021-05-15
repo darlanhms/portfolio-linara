adb7e2a03e96335e23dac449e21b752bb915a947
Darlan H <darlanhermessjb@hotmail.com>
2021-05-14 22:56:26 -0300

    ícone e titulo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,18 @@
 import Image from 'next/image';
 import React from 'react';
+import Head from 'next/head';
 
 import styles from '../styles/Home.module.css';
 
 const Home = (): React.ReactElement => (
-  <div className={styles.container}>
-    <Image width={600} height={600} src="/images/em_breve.png" />
-  </div>
+  <>
+    <Head>
+      <title>Linara Tomasoni</title>
+    </Head>
+    <div className={styles.container}>
+      <Image width={600} height={600} src="/images/em_breve.png" />
+    </div>
+  </>
 );
 
 export default Home;
