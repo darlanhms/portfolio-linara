0ee7c9d1c86494aa1492295fcb13658349640516
Darlan H <darlanhermessjb@hotmail.com>
2021-05-23 02:13:20 -0300

    fix: navegação e foto de capa

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,8 +1,24 @@
 import { useState } from 'react';
 import Image from 'next/image';
+import clsx from 'clsx';
+import { useRouter } from 'next/router';
 import Burguer from '../Burguer';
 import Menu from '../Menu';
 import styles from './styles.module.css';
+
+function isRouteActive(url: string) {
+  const router = useRouter();
+
+  return router.pathname === url;
+}
+
+function Link({ url, desc }: { url: string; desc: string }) {
+  return (
+    <a className={clsx({ [styles.active]: isRouteActive(url) })} href={url}>
+      {desc}
+    </a>
+  );
+}
 
 const Header = (): React.ReactElement => {
   const [open, setOpen] = useState<boolean>(false);
@@ -12,19 +28,19 @@ const Header = (): React.ReactElement => {
         <Menu open={open} />
         <Burguer open={open} setOpen={setOpen} />
       </div>
-      <Image width={600} height={600} src="/images/capa.png" layout="intrinsic" />
+      <Image width={700} height={460} src="/images/capa.png" layout="intrinsic" />
       <ul className={styles.links}>
         <li>
-          <a href="/">Home</a>
+          <Link url="/" desc="Home" />
         </li>
         <li>
-          <a href="/designs">Designs</a>
+          <Link url="/designs" desc="Designs" />
         </li>
         <li>
-          <a href="/illustrations">Ilustrações</a>
+          <Link url="/illustrations" desc="Ilustrações" />
         </li>
         <li>
-          <a href="/">Sobre</a>
+          <Link url="/about" desc="Sobre" />
         </li>
       </ul>
     </header>
